bf85fc4d71ad2eb824d2442d93fabee1503906c4
AdhaminRubenPomaYapur <a2019115301@estudiantes.upsa.edu.bo>
2023-05-26 14:45:31 -0400

    First commit

--- infraestructure/api/controllers/supplier.controller.ts
+++ infraestructure/api/controllers/supplier.controller.ts
@@ -18,6 +18,7 @@ export class SupplierController implements InterfaceCrud {
     }
     public POST = async (req: Request, res: Response): Promise<void> => {
         const { supplier } = req.body;
+        console.log(req.body);
         const supplierNew = await this.supplierService.postEntity(supplier);
         res.json({supplier: supplierNew});
     }
